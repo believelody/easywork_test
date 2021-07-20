346528652172b5dd665c30bc2df62282d916e139
believelody <believelody@gmail.com>
2021-07-21 01:34:49 +0200

    remove controllers directory

--- src/controllers/index.ts
+++
@@ -1,3 +0,0 @@
-import students from "./students";
-
-export default { students }
--- src/controllers/students.ts
+++
@@ -1,9 +0,0 @@
-import { Application, NextFunction } from "express";
-
-const getStudents = async (req: Request, res: Response, next: NextFunction) => {
-    // return res.status(200).json({
-    //     message: 'pong'
-    // });
-};
-
-export default { getStudents }
